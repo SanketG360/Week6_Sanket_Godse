fb85f133d731f9bad26409b96c10e827d2fa98e8
Sanket Godse <65492081+SanketG360@users.noreply.github.com>
2024-06-13 00:19:10 +0530

    Update paymentOrderController.ts

--- src/controllers/paymentOrderController.ts
+++ src/controllers/paymentOrderController.ts
@@ -3,8 +3,8 @@ import {createOrder , getOrderById} from '../services/paymentOrderService';
 
 export async function createOrderController(req: Request, res: Response) {
     try {
-      const author = await createOrder(req.body);
-      res.status(201).json(author);
+      const order = await createOrder(req.body);
+      res.status(201).json(order);
     } catch (error) {
       console.error('error creating order', error);
       res.status(500).json({error:'internal server error'});
@@ -16,8 +16,8 @@ export async function createOrderController(req: Request, res: Response) {
 export async function getOrderByIdController(req: Request, res: Response) {
     try {
       const id = req.params.id;
-      const author = await getOrderById(id);
-      res.status(200).json(author);
+      const order = await getOrderById(id);
+      res.status(200).json(order);
     } catch (error) {
       console.error('error finding order by id', error);
       res.status(500).json({error:'internal server error'});
